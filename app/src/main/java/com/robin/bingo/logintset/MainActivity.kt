bc7bacb6cac700c02f652c736580ae565c4a4d95
robin <531031061@qq.com>
2021-08-01 16:45:45 +0800

    Initial commit

---
+++ app/src/main/java/com/robin/bingo/logintset/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.robin.bingo.logintset
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.robin.bingo.login.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
